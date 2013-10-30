25d94a03effa01cc15abec93835020f850ea44dc
Jonathan Roy <joroy2@gmail.com>
2013-10-30 08:55:50 -0400

    Change git editor from vim to emacs and add missing change from previous commit

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,19 +1,19 @@
-if [[ -n $SSH_CONNECTION ]]; then
-  export PS1='%m:%3~$(git_info_for_prompt)%# '
-else
-  export PS1='%3~$(git_info_for_prompt)%# '
-fi
+#if [[ -n $SSH_CONNECTION ]]; then
+#  export PS1='%m:%3~$(git_info_for_prompt)%# '
+#else
+#  export PS1='%3~$(git_info_for_prompt)%# '
+#fi
 
-export LSCOLORS="exfxcxdxbxegedabagacad"
-export CLICOLOR=true
+#export LSCOLORS="exfxcxdxbxegedabagacad"
+#export CLICOLOR=true
 
-fpath=($ZSH/functions $fpath)
+fpath=($DOTFILES/functions $fpath)
 
-autoload -U $ZSH/functions/*(:t)
+autoload -U $DOTFILES/functions/*(:t)
 
 HISTFILE=~/.zsh_history
-HISTSIZE=10000
-SAVEHIST=10000
+HISTSIZE=100000
+SAVEHIST=100000
 
 setopt NO_BG_NICE # don't nice background tasks
 setopt NO_HUP
@@ -26,7 +26,7 @@ setopt EXTENDED_HISTORY # add timestamps to history
 setopt PROMPT_SUBST
 setopt CORRECT
 setopt COMPLETE_IN_WORD
-setopt IGNORE_EOF
+#setopt IGNORE_EOF
 
 setopt APPEND_HISTORY # adds history
 setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
@@ -39,10 +39,10 @@ setopt complete_aliases
 
 zle -N newtab
 
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
-bindkey '^[[5D' beginning-of-line
-bindkey '^[[5C' end-of-line
-bindkey '^[[3~' delete-char
-bindkey '^[^N' newtab
-bindkey '^?' backward-delete-char
+#bindkey '^[^[[D' backward-word
+#bindkey '^[^[[C' forward-word
+#bindkey '^[[5D' beginning-of-line
+#bindkey '^[[5C' end-of-line
+#bindkey '^[[3~' delete-char
+#bindkey '^[^N' newtab
+#bindkey '^?' backward-delete-char
